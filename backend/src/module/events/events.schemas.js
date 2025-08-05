2a9a90345c2b5dbba89d74c96318c98da8be507d
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2025-08-04 23:52:39 -0400

    update create events

--- backend/src/module/events/events.schemas.js
+++ backend/src/module/events/events.schemas.js
@@ -1,21 +1,12 @@
 import {date, z} from 'zod/v4';
 
-
-
-
-
-
 export const eventsSchema = z.object({
     id: z.number(),
     name: z.string(),
     description: z.string(),
     fecha_evento: z.iso.date(),
-    hora_evento: z.iso.time(),
+    hora_evento: z.string().regex(/^\d{2}:\d{2}$/),
     usuarios_id: z.number(),
     fecha_creacion: z.iso.datetime()
-
-    // fecha_evento: z.string().regex(DATE_REGEX, 'La fecha debe estar en formato DD/MM/YYYY'),
-    // hora_evento: z.string().regex(TIME_REGEX, 'La hora debe estar en formato HH:MM'),
-
 
 })
