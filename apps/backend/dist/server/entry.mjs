2951367ac5d58b0dc3e3f95a12d37f90f744e08b
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2025-09-16 21:52:47 -0400

    update

--- apps/backend/dist/server/entry.mjs
+++ apps/backend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_f8KdmA6k.mjs';
-import { manifest } from './manifest_BtGjqFct.mjs';
+import { manifest } from './manifest_CJwZ3_Kz.mjs';
 
 const serverIslandMap = new Map();;
 
