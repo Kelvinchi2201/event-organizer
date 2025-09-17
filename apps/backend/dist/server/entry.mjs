57356db78ac559326aaf6cc37ed7794691dfa5c0
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2025-09-17 00:38:59 -0400

    feat: scroll bugs fixed

--- apps/backend/dist/server/entry.mjs
+++ apps/backend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_f8KdmA6k.mjs';
-import { manifest } from './manifest_BRptJoi_.mjs';
+import { manifest } from './manifest_CyBUhyS2.mjs';
 
 const serverIslandMap = new Map();;
 
