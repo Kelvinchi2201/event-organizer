df18daba0cb9e4d9d5383b7c02fce2616e03eea4
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2025-09-17 00:55:42 -0400

    update

--- apps/backend/dist/server/pages/verifyguest/_token_.astro.mjs
+++ apps/backend/dist/server/pages/verifyguest/_token_.astro.mjs
@@ -1,6 +1,6 @@
 import { e as createComponent, k as renderComponent, l as renderScript, r as renderTemplate, m as maybeRenderHead } from '../../chunks/astro/server_DdhIxf7_.mjs';
 import 'kleur/colors';
-import { $ as $$Layout } from '../../chunks/Layout_DPtbRMU6.mjs';
+import { $ as $$Layout } from '../../chunks/Layout_DQ904N2B.mjs';
 export { renderers } from '../../renderers.mjs';
 
 const prerender = false;
