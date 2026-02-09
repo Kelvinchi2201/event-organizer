02a6b0cdb9eb62546c7d9ab634603110b0adda93
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2026-02-09 18:31:41 -0400

    fixed error

--- apps/backend/src/db/index.js
+++ apps/backend/src/db/index.js
@@ -1,11 +1,7 @@
 import { Client } from 'pg';
 const connectionString =
   process.env.NODE_ENV === 'dev' ? process.env.DATABASE_DEV_URL : process.env.DATABASE_URL;
-const db = new Client({ 
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = new Client({ connectionString });
+
 await db.connect();
 export default db;
