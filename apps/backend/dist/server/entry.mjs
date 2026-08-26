70f28d76e46521874f6884df31461b1622ab8168
Kelvinchi2201 <kelvinchirinos2702@gmail.com>
2026-08-26 18:57:44 -0400

    added comments module to prod

--- apps/backend/dist/server/entry.mjs
+++ apps/backend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_f8KdmA6k.mjs';
-import { manifest } from './manifest_Fg-ly71K.mjs';
+import { manifest } from './manifest_Dh-utgm7.mjs';
 
 const serverIslandMap = new Map();;
 
